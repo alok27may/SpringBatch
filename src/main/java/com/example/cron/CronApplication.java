44b6155fb1ac20db3ad94c9ff15efb147c54b656
Kumar, Alok <Alok.Kumar@cdk.com>
2018-10-01 15:46:41 +0530

    First Commit

---
+++ src/main/java/com/example/cron/CronApplication.java
@@ -0,0 +1,12 @@
+package com.example.cron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronApplication.class, args);
+	}
+}
